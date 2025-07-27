afffe29edc312627d5c742164689602e1fa3ba88
Shehryar Aijaz <shehryar120345@gmail.com>
2025-07-27 21:23:58 +0500

    feat: integrate Cloudinary and Multer for file uploads, update .env.example with Cloudinary configuration, and enhance package dependencies in package.json and package-lock.json.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+(async function () {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINAY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    })
+})
+
+const uploadHandler = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        // Upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(
+            localFilePath,
+            {
+                resource_type: "auto",
+            }
+        )
+        console.log("File uploaded successfully", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.log("Error uploading file", error);
+        return null;
+    }
+}
+
+export default uploadHandler;
