e21862eaaf3d16c5e876f45f0df677119aecad53
Shehryar Aijaz <shehryar120345@gmail.com>
2025-07-29 17:24:59 +0500

    feat: enhance user controller with password change, account update, and avatar/cover image upload functionalities; add subscription model for user channel subscriptions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -114,8 +114,8 @@ const logoutUser = asyncHandler( async(req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: null
+            $unset: {
+                refreshToken: undefined
             }
         },
         {
@@ -151,7 +151,128 @@ const refreshAccessToken = asyncHandler( async(req, res) => {
     cookie("accessToken", newAccessToken, options).
     cookie("refreshToken", newRefreshToken, options).
     json(new ApiResponse(200, { accessToken: newAccessToken, refreshToken: newRefreshToken }, "Access token refreshed successfully"))
-    
 } )
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changeCurrentUserPassword = asyncHandler( async(req, res) => {
+    const { currentPassword, newPassword } = req.body;
+
+    if (!(currentPassword || newPassword)) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findById(req.user?._id);
+
+    const isPasswordCorrect = await user.isPasswordCorrect(currentPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "Invalid current password")
+    }
+
+    user.password = newPassword;
+
+    await user.save({ validateBeforeSave: false })
+
+    return res.status(200).
+    json(new ApiResponse(200, null, "Password changed successfully"))
+} )
+
+const getCurrentUser = asyncHandler( async(req, res) => {
+    return res.status(200).
+    json(new ApiResponse(200, req.user, "Current user fetched successfully"))
+} )
+
+const updateAccountDetails = asyncHandler( async(req, res) => {
+    const { fullName, email } = req.body;
+
+    if (!(fullName || email)) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email
+            }
+        },
+        { new: true }
+    ).select("-password -refreshToken")
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    return res.status(200).
+    json(new ApiResponse(200, user, "Account details updated successfully"))
+} )
+
+const updateUserAvatar = asyncHandler( async(req, res) => {
+    const avatarLocalPath = req.file?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatarUpload = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatarUpload) {
+        throw new ApiError(400, "Avatar upload failed")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatarUpload?.url
+            }
+        },
+        { new: true }
+    ).select("-password -refreshToken")
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    return res.status(200).
+    json(new ApiResponse(200, { avatar: avatarUpload?.url }, "Avatar updated successfully"))
+} )
+
+const updateUserCoverImage = asyncHandler( async(req, res) => {
+    const coverImageLocalPath = req.file?.path;
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image is required")
+    }
+
+    const coverImageUpload = await uploadOnCloudinary(coverImageLocalPath);
+    
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImageUpload?.url
+            }
+        },
+        { new: true }
+    ).select("-password -refreshToken")
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    return res.status(200).
+    json(new ApiResponse(200, { coverImage: coverImageUpload?.url }, "Cover image updated successfully"))
+} )
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentUserPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+};
