b32c938f141f8930e9edbfba633459dade1965ac
Shehryar Aijaz <shehryar120345@gmail.com>
2025-07-29 14:50:45 +0500

    feat: implement JWT authentication for user login and logout; add middleware for token verification and enhance user controller with token generation logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,26 @@ import ApiResponse from '../utils/ApiResponse.js';
 import { User } from '../models/user.model.js';
 import uploadOnCloudinary from '../utils/cloudinary.js';
 
+const generateTokens = async (userId) => {
+    try{
+        const user = await User.findById(userId)
+
+        if (user) {
+            const accessToken = user.generateAccessToken()
+            const refreshToken = user.generateRefreshToken()
+
+            user.refreshToken = refreshToken
+            await user.save({ validateBeforeSave: false })
+
+            return { accessToken, refreshToken }
+        }
+
+        return null
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens")
+    }
+}
+
 const registerUser = asyncHandler( async(req, res) => {
     const { username, email, fullName, password } = req.body;
 
@@ -64,14 +84,59 @@ const loginUser = asyncHandler( async(req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = await User.findOne({ username: username.toLowerCase() })
+    const user = await User.findOne({
+        $or: [{ username: username.toLowerCase() },
+            { email: username.toLowerCase() }]
+        })
     console.log(user);
 
     if (!user) {
         throw new ApiError(404, "User not found")
     }
 
-    return res.status(200).json(new ApiResponse(200, user, "User logged in successfully"))
+    const isPasswordCorrect = await user.isPasswordCorrect(password)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "Invalid password")
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).
+    cookie("accessToken", accessToken, { ...options, maxAge: 1000 * 60 * 15 }).
+    cookie("refreshToken", refreshToken, { ...options, maxAge: 1000 * 60 * 60 * 24 * 30 }).
+    json(new ApiResponse(200, { user: loggedInUser, accessToken, refreshToken }, "User logged in successfully"))
 } )
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler( async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).
+    clearCookie("accessToken", { ...options, maxAge: 0 }).
+    clearCookie("refreshToken", { ...options, maxAge: 0 }).
+    json(new ApiResponse(200, null, "User logged out successfully"))
+} )
+
+export { registerUser, loginUser, logoutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { User } from "../models/user.model";
+import asyncHandler from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler( async(req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.split(" ")[1]
+    
+        if (!token) {
+            throw new ApiError(401, "Unauthorized")
+        }
+    
+        const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        if (!decoded) {
+            throw new ApiError(401, "Unauthorized")
+        }
+    
+        const user = await User.findById(decoded._id).select("-password -refreshToken")
+    
+        if (!user) {
+            throw new ApiError(401, "Unauthorized")
+        }
+    
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while verifying JWT")
+    }
+} )
