cedc9813aff09e2f690f4a88427d8792d14aaad7
Shehryar Aijaz <shehryar120345@gmail.com>
2025-07-30 21:42:51 +0500

    feat: initialize frontend with React and Vite; add Tailwind CSS for styling, ESLint configuration for code quality, and basic application structure including main component and entry point

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-gray-900 flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold text-white hover:text-red-500 transition-all duration-300">Ongoing Backend & Frontend Project</h1>
+    </div>
+  )
+}
+
+export default App;
