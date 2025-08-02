9cd476db58dd2924e7c3129eb7d1013b99a24f8a
Shehryar Aijaz <shehryar120345@gmail.com>
2025-08-02 08:34:43 +0500

    feat: initialize frontend structure with configuration files, routing, and basic components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
-
+import AppRouter from "./router/AppRouter";
 function App() {
 
-  return (
-    <div className="bg-gray-900 flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold text-white hover:text-red-500 transition-all duration-300">Ongoing Backend & Frontend Project</h1>
-    </div>
-  )
+  return <AppRouter />
 }
 
 export default App;
