08e39120c1602c5ae04cbe53c92bf559d3930a50
Shehryar Aijaz <shehryar120345@gmail.com>
2025-08-05 04:33:04 +0500

    chore: improve code formatting

--- frontend/src/features/auth/api.js
+++ frontend/src/features/auth/api.js
@@ -1,68 +1,67 @@
-import api from '@/services/api.js';
+import api from "@/services/api.js";
 
 export const loginUser = async ({ username, email, password }) => {
-    try {
-        const response = await api.post('/users/login', {
-            username,
-            email,
-            password
-        })
+  try {
+    const response = await api.post("/users/login", {
+      username,
+      email,
+      password,
+    });
 
-        if (response?.status === 200) {
-            localStorage.setItem('user', JSON.stringify(response.data.user));
-            return response.data;
-        } else {
-            throw new Error(response.data.message);
-        }
-
-    } catch (error) {
-        if (error?.message) {
-            throw error
-        } else if (error?.data?.message) {
-            throw new Error(error.data.message)
-        } else {
-            throw new Error('Login failed')
-        }
+    if (response?.status === 200) {
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      return response.data;
+    } else {
+      throw new Error(response.data.message);
     }
-}
+  } catch (error) {
+    if (error?.message) {
+      throw error;
+    } else if (error?.data?.message) {
+      throw new Error(error.data.message);
+    } else {
+      throw new Error("Login failed");
+    }
+  }
+};
 
 export const registerUser = async (formData) => {
-    try {
-        const response = await api.post('/users/register', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        })
+  try {
+    const response = await api.post("/users/register", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
 
-        if (response?.status === 201) {
-            localStorage.setItem('user', JSON.stringify(response.data.data))
-            return response.data
-        } else {
-            throw new Error(response.data.message)
-        }
-    } catch (error) {        
-        if (error?.message) {
-            throw new Error(error.message)
-        } else {
-            throw new Error("Registration failed")
-        }
+    if (response?.status === 201) {
+      localStorage.setItem("user", JSON.stringify(response.data.data));
+      return response.data;
+    } else {
+      throw new Error(response.data.message);
     }
-}
+  } catch (error) {
+    if (error?.message) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("Registration failed");
+    }
+  }
+};
 
 export const logoutUser = async () => {
-    try{
-        const response = await api.post('/users/logout')
-        if (response?.status === 200) {
-            localStorage.removeItem('user')
-            return response.data
-        } else {
-            throw new Error(response.data.message)
-        }
-    } catch (error) {
-        if (error?.message) {
-            throw new Error(error.message)
-        } else {
-            throw new Error('Logout failed')
-        }
+  try {
+    const response = await api.post("/users/logout");
+    if (response?.status === 200) {
+      localStorage.removeItem("user");
+      return response.data;
+    } else {
+      throw new Error(response.data.message);
     }
-}
+  } catch (error) {
+    if (error?.message) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("Logout failed");
+    }
+  }
+};
